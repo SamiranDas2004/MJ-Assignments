5fbdad0cda70610c65a40bc440b260e6359eaed3
Samiran_Das <samiran4209@gmail.com>
2024-07-26 00:00:09 +0530

    Update CoustomRoutes.jsx

--- assignment01/src/routes/CoustomRoutes.jsx
+++ assignment01/src/routes/CoustomRoutes.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import OtpForm from '../components/otpPage/OtpForm'
-import CourseList from '../components/CourseLIst/CourseList'
-import Batches from '../components/batches/Batches'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import OtpForm from '../components/otpPage/OtpForm';
+import CourseList from '../components/CourseLIst/CourseList';
+import Batches from '../components/batches/Batches';
 
 function CoustomRoutes() {
   return (
     <>
-   <Routes>
-    <Route path='/' element={<OtpForm/>}/>
-    <Route path='course-list' element={<CourseList/>}/>
-    <Route path='batches' element={<Batches/>}/>
-   </Routes>
-   </>
-  )
+      <Routes>
+        <Route path='/otp-form' element={<OtpForm />} />
+        <Route path='/course-list' element={<CourseList />} />
+        <Route path='/batches' element={<Batches />} />
+        <Route path='*' element={<OtpForm />} /> 
+      </Routes>
+    </>
+  );
 }
 
-export default CoustomRoutes
+export default CoustomRoutes;
